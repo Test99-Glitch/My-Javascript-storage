cfe1858d7767ae22bf69ad4352e615e01b190b26
Glitch <142440946+Test99-Glitch@users.noreply.github.com>
2023-11-02 20:49:56 -0400

    Update JavaScriptPlus.js

--- JavaScriptPlus.js
+++ JavaScriptPlus.js
@@ -2,21 +2,21 @@ function DefineJavaScriptPlus() {
    var JSP = function(Code) {
       var result = [];
       var TypeScript = document.createElement('script');
-TypeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
-TypeScript.onload = function() {
-   result.push(ts.transpile(Code));
-};
+      TypeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
+      TypeScript.onload = function() {
+         result.push(ts.transpile(Code));
+      };
       var CoffeeScript = document.createElement('script');
-CoffeeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
-CoffeeScript.onload = function() {
-   result.push(CoffeeScript.compile(Code));
-};
-document.body.appendChild(TypeScript);
-document.body.appendChild(CoffeeScript);
-result = result.replace(',', '');
-eval(result);
-TypeScript.remove();
-CoffeeScript.remove();
+      CoffeeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
+      CoffeeScript.onload = function() {
+         result.push(CoffeeScript.compile(Code));
+      };
+      document.body.appendChild(TypeScript);
+      document.body.appendChild(CoffeeScript);
+      result = result.join('');
+      eval(result);
+      TypeScript.remove();
+      CoffeeScript.remove();
    };
    return JSP;
 }
