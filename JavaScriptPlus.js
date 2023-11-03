fb176d3a1fcfa0d7d63ab8b009823a88de761ee1
Glitch <142440946+Test99-Glitch@users.noreply.github.com>
2023-11-02 20:53:18 -0400

    Update JavaScriptPlus.js

--- JavaScriptPlus.js
+++ JavaScriptPlus.js
@@ -1,23 +1,30 @@
 function DefineJavaScriptPlus() {
-   var JSP = function(Code) {
-      var result = [];
-      var TypeScript = document.createElement('script');
-      TypeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
-      TypeScript.onload = function() {
-         result.push(ts.transpile(Code));
-      };
-      var CoffeeScript = document.createElement('script');
-      CoffeeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
-      CoffeeScript.onload = function() {
-         result.push(CoffeeScript.compile(Code));
-      };
-      document.body.appendChild(TypeScript);
-      document.body.appendChild(CoffeeScript);
-      result = result.join('');
-      eval(result);
-      TypeScript.remove();
-      CoffeeScript.remove();
-   };
-   return JSP;
+  var JSP = function(Code) {
+    var result = [];
+
+    var TypeScript = document.createElement('script');
+    TypeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
+    TypeScript.onload = function() {
+      result.push(ts.transpile(Code));
+    };
+
+    var CoffeeScript = document.createElement('script');
+    CoffeeScript.src = 'https://cdn.jsdelivr.net/npm/coffeescript@latest/extras/coffeescript.js';
+    CoffeeScript.onload = function() {
+      result.push(CoffeeScript.compile(Code));
+    };
+
+    document.body.appendChild(TypeScript);
+    document.body.appendChild(CoffeeScript);
+
+    result = result.join('');
+    eval(result);
+
+    TypeScript.remove();
+    CoffeeScript.remove();
+  };
+
+  return JSP;
 }
+
 window.JSP = DefineJavaScriptPlus();
