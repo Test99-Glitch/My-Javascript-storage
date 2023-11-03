5f777e001ebda28ca8f4983630c348d5ff719412
Glitch <142440946+Test99-Glitch@users.noreply.github.com>
2023-11-02 21:16:29 -0400

    Update JavaScriptPlus.js

--- JavaScriptPlus.js
+++ JavaScriptPlus.js
@@ -1,30 +1,164 @@
 function DefineJavaScriptPlus() {
-  var JSP = function(Code) {
-    var result = [];
-
-    var TypeScript = document.createElement('script');
-    TypeScript.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
-    TypeScript.onload = function() {
-      result.push(ts.transpile(Code));
-    };
-
-    var CoffeeScript = document.createElement('script');
-    CoffeeScript.src = 'https://cdn.jsdelivr.net/npm/coffeescript@latest/extras/coffeescript.js';
-    CoffeeScript.onload = function() {
-      result.push(CoffeeScript.compile(Code));
-    };
-
-    document.body.appendChild(TypeScript);
-    document.body.appendChild(CoffeeScript);
-
-    result = result.join('');
-    eval(result);
-
-    TypeScript.remove();
-    CoffeeScript.remove();
+  var JSP = {};
+  JSP.LS = function(Link, Id) {
+    var LoadScript = document.createElement('script');
+    LoadScript.src = Link;
+    LoadScript.id = Id;
+    document.body.appendChild(LoadScript);
   };
-
+  JSP.RS = function(Id) {
+    var RemoveScript = document.getElementById(Id);
+    if (RemoveScript) {
+      if (RemoveScript.tagName === 'script') {
+        RemoveScript.remove();
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
+  };
+  JSP.E = function(Type) {
+    if (!(typeof Type === 'boolean') && !(Type === 'toggle')) {
+      return;
+    } else if (typeof Type === 'boolean') {
+      document.documentElement.contentEditable = Type;
+    } else if (Type === 'toggle') {
+      document.documentElement.contentEditable = !document.documentElement.contentEditable;
+    }
+  };
+  JSP.EE = function(Element, Type) {
+    if (typeof Type === 'boolean') {
+      Element.contentEditable = Type;
+    } else if (Type === 'toggle') {
+      Element.contentEditable = !Element.contentEditable;
+    }
+  };
+  JSP.AV = function(N, V) {
+    window[N] = V;
+  };
+  JSP.RV = function(N) {
+    delete window[N];
+  };
+  JSP.GV = function(N) {
+    if (typeof window[N] === 'undefined') {
+      return;
+    } else {
+      return window[N];
+    }
+  };
+  JSP.EAD = function(Text, Type) {
+    if (Type !== 'encode' && Type !== 'decode') {
+      return;
+    } else if (Type === 'encode') {
+      return encodeURIComponent(Text);
+    } else if (Type === 'decode') {
+      return decodeURIComponent(Text);
+    }
+  };
+  JSP.LL = function(Link) {
+    if (Link) {
+      var LoadLink = document.createElement('a');
+      LoadLink.href = Link;
+      document.body.appendChild(LoadLink);
+      LoadLink.click();
+      LoadLink.remove();
+    } else {
+      return;
+    }
+  };
+  JSP.CT = function() {
+    var tooltip = document.querySelector('#JavaScriptPlus-Tooltip');
+    if (!tooltip) {
+      tooltip = document.createElement('div');
+      tooltip.id = 'JavaScriptPlus-Tooltip';
+      tooltip.style.position = 'fixed';
+      tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+      tooltip.style.color = 'white';
+      tooltip.style.padding = '5px';
+      tooltip.style.borderRadius = '5px';
+      tooltip.style.zIndex = '9999';
+      tooltip.style.pointerEvents = 'none';
+      document.body.appendChild(tooltip);
+    }
+    document.addEventListener('mousemove', function (e) {
+      var target = e.target;
+      var tagName = target.tagName.toLowerCase();
+      var id = target.id;
+      var classes = Array.from(target.classList).join(', ');
+      if (tagName || id || classes) {
+        tooltip.textContent = `Tag: ${tagName}\nID: ${id}\nClasses: ${classes}`;
+        tooltip.style.left = e.clientX + 'px';
+        tooltip.style.top = e.clientY + 'px';
+      }
+    });
+  };
+  JSP.TT = function() {
+    var tooltip = document.getElementById('JavaScriptPlus-Tooltip');
+    if (tooltip) {
+      if (tooltip.style.display === 'block') {
+        tooltip.style.display = 'none';
+      } else {
+        tooltip.style.display = 'block';
+      }
+    } else {
+      return;
+    }
+  };
+  JSP.S = function(Type) {
+    if (Type === 'save') {
+      localStorage.setItem('JavaScriptPlus S', document.documentElement.innerHTML);
+    } else if (Type === 'load') {
+      var savedContent = localStorage.getItem('JavaScriptPlus S');
+      if (savedContent !== null) {
+        document.documentElement.innerHTML = savedContent;
+      } else {
+        return;
+      }
+    }
+  };
+  JSP.JS = function(Code) {
+    eval(Code);
+  };
+  JSP.HTML = function(Head, Body) {
+    document.head.innerHTML = Head;
+    document.body.innerHTML = Body;
+  };
+  JSP.CSS = function(Code) {
+    var style = document.createElement('style');
+    style.innerHTML = Code;
+    document.documentElement.appendChild(style);
+  };
+  JSP.TS = function(Code) {
+    var code = Code;
+    var Script = document.createElement('script');
+    Script.src = 'https://cdn.jsdelivr.net/npm/typescript@latest/lib/typescript.js';
+    Script.onload = function() {
+      var result = ts.transpile(code);
+      eval(result);
+    };
+    document.body.appendChild(Script);
+    Script.remove();
+  };
+  JSP.CS = function(Code) {
+    var code = Code;
+    var Script = document.createElement('script');
+    Script.src = 'https://cdn.jsdelivr.net/npm/coffeescript@latest/extras/coffeescript.js';
+    Script.onload = function() {
+      var result = CoffeeScript.compile(code);
+      eval(result);
+    };
+    document.body.appendChild(Script);
+    Script.remove();
+  };
+  JSP.T = function(Code) {
+    var Test = eval(Code);
+    if (Test === Error) {
+      return 'error';
+    } else {
+      return 'no error';
+    }
+  };
   return JSP;
 }
-
 window.JSP = DefineJavaScriptPlus();
