1f00c546f66ae9685741fbf29ab2adbb739c5597
Glitch <142440946+Test99-Glitch@users.noreply.github.com>
2023-11-06 08:42:50 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,26 +1,3 @@
-(function (window) {
-    var parseCustomSyntax = function(code) {
-        var regex = /test((.*?))/g;
-        var matches = code.match(regex);
-
-        if (matches) {
-            for (var i = 0; i < matches.length; i++) {
-                var match = matches[i];
-                var content = match.slice(2, -2);
-                var FinishedCode = eval(content);
-                if (FinishedCode == error) {
-                return 'error';
-                } else {
-                return 'no error';
-                }
-            }
-        }
-    };
-    var myLibrary = {
-        processCustomSyntax: function(code) {
-            parseCustomSyntax(code);
-        }
-    };
-    window.myLibrary = myLibrary;
-
-})(window);
+window.HelloWorld = function() {
+    alert("Hello World");
+};
